a71d9a61974bb0480c547a3871b115bb837e9387
ducban1108 <levietduc1108@gmail.com>
2018-10-30 13:38:01 +0700

    aaa

--- app/src/main/java/ducban/deptrai/comot/khonghai/projectandroid/MainActivity.java
+++ app/src/main/java/ducban/deptrai/comot/khonghai/projectandroid/MainActivity.java
@@ -2,15 +2,19 @@ package ducban.deptrai.comot.khonghai.projectandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
