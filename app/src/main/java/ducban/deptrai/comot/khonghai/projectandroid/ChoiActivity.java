f42164c0031dc0ddebb79da9134951bdb3b2f7c9
ducban1108 <levietduc1108@gmail.com>
2018-11-08 13:07:46 +0700

    Chỉnh sửa giao diện. Done!!! Hẳn hoi

---
+++ app/src/main/java/ducban/deptrai/comot/khonghai/projectandroid/ChoiActivity.java
@@ -0,0 +1,25 @@
+package ducban.deptrai.comot.khonghai.projectandroid;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChoiActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choi);
+
+    }
+
+
+
+
+
+
+}
